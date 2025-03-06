1bb2b43eff14f505cb057c5fe749a9034a355645
mahee <mfernando@controlunion.com>
2025-03-06 16:28:13 +0530

    home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Hero } from '../components/Hero'
+import Features from '../components/Features'
+import NewArrivals from '../components/NewArrivals'
+import PopularProducts from '../components/PopularProducts'
+import Banner from '../components/Banner'
+import About from '../components/About'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
+import Blog from './Blog'
+
+export const Home = () => {
+  return (
+    <>
+    <Hero />
+    <Features />
+    <NewArrivals />
+    <PopularProducts />
+    <Banner />
+    <About />
+    <Footer />
+    </>
+  )
+}
