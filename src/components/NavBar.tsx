d0c210f529d9108b1d4c134c1fcec6a75c79e53c
mahee <mfernando@controlunion.com>
2025-03-06 16:48:13 +0530

    creating nav bar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+interface NavBarProps {
+  containerStyles: string;
+}
+
+export const NavBar: React.FC<NavBarProps> = ({ containerStyles,onClick }) => {
+
+  const navLinks = [
+    { path: '/', title: 'Home' },
+    { path: '/collection', title: 'Collection' },
+    { path: '/blog', title: 'Blog' },
+    { path: '/mail', title: 'Contact' },
+  ]
+
+  return (
+    <nav className={`${containerStyles}`}>
+      {navLinks.map((link) => (
+        <NavLink
+          key={link.title}
+          to={link.path}
+          className={({ isActive }) =>
+            `${isActive ? 'active-link' : ''} px-3 py-2 rounded-full`
+          }
+          onClick={onClick}  //Closed menu when link is clicked
+        >
+          {link.title}
+        </NavLink>
+      ))}
+    </nav>
+  )
+}
