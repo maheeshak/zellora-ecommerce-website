4dc11436cfa4a5d3f78d1aa1378d9b1106511bf7
mahee <mfernando@controlunion.com>
2025-03-06 16:48:37 +0530

    creating new arrivals component

---
+++ src/components/NewArrivals.tsx
@@ -0,0 +1,79 @@
+import React, { useContext, useEffect, useState } from 'react'
+// import products from '../path/to/products' // Adjust the import path as necessary
+import Title from './Title'
+import 'swiper/css';
+
+
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Item from './Item';
+
+import { ShopContext } from './context/ShopContext';
+
+
+
+const NewArrivals = () => {
+ const {products}:any = useContext(ShopContext);
+
+const [PopularProducts, setPopularProducts] = useState<any[]>([]);
+ useEffect(() => {
+  const data = products.slice(0, 7);
+  setPopularProducts(data);
+ },[products]);
+  return (
+    <section className='max-padd-container pt-16'>
+      <Title title={'New'} title2={'Arrivals'} titleStyles={'pb-10'} paraStyles={'!block !pb-10'}/>
+      {/* Container */}
+      <Swiper
+      autoplay={{
+        delay:2500,
+        disableOnInteraction:false,
+      }}
+      breakpoints={{
+        300:{
+          slidesPerView:2,
+          spaceBetween:30,
+        },
+        666:{
+          slidesPerView:3,
+          spaceBetween:30,
+        },
+        900:{
+          slidesPerView:4,
+          spaceBetween:30, 
+        },
+        1300:{
+          slidesPerView:5,
+          spaceBetween:30,
+        },
+      }}
+      
+        modules={[Autoplay]}
+        className="h-[500 px] mt-5"
+      >{PopularProducts.map((product)=>(
+<SwiperSlide key={product._id}>
+          <Item product={product}/>
+        </SwiperSlide>
+
+      ))
+      
+      
+      
+      
+      }
+ 
+      
+       
+       
+      </Swiper>
+
+    </section>
+  )
+}
+
+export default NewArrivals
